a4ed043fd46242082d0d9a0ce0312499d75c80f1
Charles Anthony <anthony@nu-it.at>
2022-11-09 10:47:36 +0100

    added latest

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-
+import { getFirestore } from "firebase/firestore"
+import { collectionGroup, collection, getDocs, addDoc, doc , setDoc } from "firebase/firestore";
+import { query, where, limit , orderBy } from "firebase/firestore"
 
 
 
@@ -26,9 +28,68 @@ var firebaseConfig = {
   };
 
 
+
 firebase.initializeApp(firebaseConfig);
 
 const app = createApp(App);
+
+var database = getFirestore();
+
+
+
+// example data
+
+var citiesRef = collection(database, "users")
+
+await setDoc(doc(citiesRef, "SF"), {
+  name: "San Francisco", state: "CA", country: "USA",
+  capital: false, population: 860000,
+  regions:["west_coast", "norcal"]
+})
+
+await setDoc(doc(citiesRef,"WA"), {
+  name: "Wattala", state : "WA", country: "Srilanka",
+  capital: false, population: 100000,
+  regions: ["elakanda", "hendala"]
+})
+
+
+var citiesRef = collection(database, "users")
+
+// create a query against the collection
+
+// collection group queries
+var animalsRef = collection(database, "animals")
+
+await Promise.all([
+  addDoc(collection(animalsRef, "SF","landmarks"), {
+    name: "Golden gate bridge",
+    type: "bridge"
+  }),
+
+  addDoc(collection(animalsRef,"SF", "landmarks"), {
+    name: "Legion of honor",
+    type: "museum"
+  }),
+  addDoc(collection(animalsRef, "TX", "new site"), {
+    name: "legion of honor",
+    type: "tower"
+  })
+])
+
+
+// var museums = query(collectionGroup(database, "landmarks"), where("type", "==", "museum"))
+// var querySnapshot = await getDocs(museums)
+// querySnapshot.forEach((doc)=> {
+//   console.log(doc.id, '=>', doc.data())
+// })
+
+
+
+
+
+
+
 
 app.use(router);
 
